0cdabe970a4d7dd19fb7e5eb8c45399621762762
Raman Yaraslautsau <s22349@pjwstk.edu.pl>
2024-05-20 13:51:09 +0200

    Push APBD8

---
+++ LinqTasks/LinqTutorials/LinqTasks/Extensions/CustomExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using LinqTasks.Models;
+
+namespace LinqTasks.Extensions;
+
+public static class CustomExtensionMethods
+{
+    public static IEnumerable<Emp> GetEmpsWithSubordinates(this IEnumerable<Emp> emps)
+    {
+        return emps
+            .Where(emp => emps.Any(subordinate => subordinate.Mgr == emp))
+            .OrderBy(emp => emp.Ename)
+            .ThenByDescending(emp => emp.Salary);
+    }
+}
